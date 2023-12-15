4a82ce4b0c8a8e664628cedab89cee498ded4bf3
icyn <apartmine@outlook.com>
2023-12-15 10:27:04 +0800

    fix:linux打包

--- vue.config.js
+++ vue.config.js
@@ -29,4 +29,21 @@ module.exports = {
       return args;
     });
   },
+  pluginOptions: {
+    electronBuilder: {
+      customFileProtocol: "./",
+      builderOptions: {
+        appId: "com.example.app",
+        productName: "project", //项目名，也是生成的安装文件名，即aDemo.exe
+        copyright: "Copyright © 2023", //版权信息
+        directories: {
+          output: "./dist", //输出文件路径
+        },
+        linux: {
+          icon: "./public/favicon.ico", // 注意linux下图片的尺寸最好是256*256
+          target: "deb", // 这个字段也可以是数组格式，具体可以参考electron-builder官网
+        },
+      },
+    },
+  },
 };
